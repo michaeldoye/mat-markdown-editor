312472f72fe49a52bc0b89f5bd1c2b7c235f247c
Michael Doye <mdoye@mobiquityinc.com>
2019-03-18 22:51:07 +0100

    chore(lib): code refactor and clean up
    Moved some things hither and thither

--- demo/src/app/app.component.spec.ts
+++ demo/src/app/app.component.spec.ts
@@ -1,39 +1,42 @@
 /* tslint:disable:no-unused-variable */
-import { Router } from '@angular/router';
-import { Component } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { Router } from '@angular/router'
+import { Component } from '@angular/core'
+import { TestBed, async } from '@angular/core/testing'
+import { AppComponent } from './app.component'
+import { FooterComponent } from './shared/footer/footer.component'
 
-import { RouterOutletStubComponent, RouterLinkStubDirective } from '../testing/router-stubs';
-import { Observable, of } from 'rxjs';
+import {
+  RouterOutletStubComponent,
+  RouterLinkStubDirective,
+} from '../testing/router-stubs'
+import { Observable, of } from 'rxjs'
 
 @Component({ selector: 'app-header', template: '' })
-class HeaderStubComponent { }
+class HeaderStubComponent {}
 
 class RouterStub {
-  events: Observable<Event> = of<Event>();
+  events: Observable<Event> = of<Event>()
 }
 
+// @ts-ignore
 describe('AppComponent', () => {
+  // @ts-ignore
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         HeaderStubComponent,
         FooterComponent,
-        RouterOutletStubComponent
+        RouterOutletStubComponent,
       ],
-      providers: [
-        { provide: Router, useClass: RouterStub }
-      ]
-    });
-  }));
-
+      providers: [{ provide: Router, useClass: RouterStub }],
+    })
+  }))
+  // @ts-ignore
   it('should create', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-});
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.debugElement.componentInstance
+    // @ts-ignore
+    expect(app).toBeTruthy()
+  }))
+})
--- src/module/editor/editor.component.ts
+++ src/module/editor/editor.component.ts
@@ -1,196 +1,165 @@
 import {
-    Component,
-    ViewChild,
-    forwardRef,
-    Attribute,
-    Input,
-    AfterViewInit,
-    OnInit,
-    OnDestroy,
-    ElementRef,
+  Component,
+  ViewChild,
+  forwardRef,
+  Attribute,
+  Input,
+  AfterViewInit,
+  OnInit,
+  OnDestroy,
+  ElementRef,
 } from '@angular/core';
 import {
-    NG_VALUE_ACCESSOR,
-    ControlValueAccessor,
-    NG_VALIDATORS,
-    Validator,
-    AbstractControl,
-    ValidationErrors,
+  NG_VALUE_ACCESSOR,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  Validator,
+  AbstractControl,
+  ValidationErrors,
 } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatMarkdownEditorOptions } from '../lib.interface';
+import { markDownCode, markDownTable, markDownListItem } from '../utils';
 
 declare let ace: any;
 declare let marked: any;
 declare let hljs: any;
 
 @Component({
-    // tslint:disable-next-line: component-selector
-    selector: 'mat-markdown-editor',
-    templateUrl: './editor.component.html',
-    styleUrls: ['./editor.component.scss'],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => MatMarkdownEditorComponent),
-            multi: true,
-        },
-        {
-            provide: NG_VALIDATORS,
-            useExisting: forwardRef(() => MatMarkdownEditorComponent),
-            multi: true,
-        },
-    ],
+  // tslint:disable-next-line: component-selector
+  selector: 'mat-markdown-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => MatMarkdownEditorComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => MatMarkdownEditorComponent),
+      multi: true,
+    },
+  ],
 })
 export class MatMarkdownEditorComponent
-    implements
-        ControlValueAccessor,
-        Validator,
-        OnInit,
-        AfterViewInit,
-        OnDestroy {
-    @ViewChild('aceEditor') public aceEditorContainer: ElementRef;
-    @Input() public options: MatMarkdownEditorOptions;
+  implements ControlValueAccessor, Validator, OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('aceEditor') public aceEditorContainer: ElementRef;
+  @Input() public options: MatMarkdownEditorOptions;
 
-    public showPreviewPanel = true;
-    public isFullScreen = false;
-    public previewHtml: any;
-    public sliderHeight: any;
-    public editor: any;
+  public showPreviewPanel = true;
+  public isFullScreen = false;
+  public previewHtml: any;
+  public editor: any;
 
-    private _markdownValue: any;
-    private _options: any = {};
-    private _markedOpt: any;
+  private _renderMarkTimeout: any;
+  private _markdownValue: any;
+  private _options: any = {};
+  private _markedOpt: any;
 
-    private _onChange = (_: any) => {};
-    private _onTouched = () => {};
+  private _onChange = (_: any) => {};
+  private _onTouched = () => {};
 
-    public get markdownValue(): any {
-        return this._markdownValue || '';
+  public get markdownValue(): any {
+    return this._markdownValue || '';
+  }
+  public set markdownValue(value: any) {
+    this._markdownValue = value;
+    this._onChange(value);
+
+    if (this.options.preRender && this.options.preRender instanceof Function) {
+      value = this.options.preRender(value);
     }
-    public set markdownValue(value: any) {
-        this._markdownValue = value;
-        this._onChange(value);
-
-        if (
-            this.options.preRender &&
-            this.options.preRender instanceof Function
-        ) {
-            value = this.options.preRender(value);
-        }
-        if (value) {
-            const html = marked(value || '', this._markedOpt);
-            this.previewHtml = this._domSanitizer.bypassSecurityTrustHtml(html);
-        }
+    if (value !== null && value !== undefined) {
+      if (this._renderMarkTimeout) {
+        clearTimeout(this._renderMarkTimeout);
+      }
+      this._renderMarkTimeout = setTimeout(() => {
+        const html = marked(value || '', this._markedOpt);
+        this.previewHtml = this._domSanitizer.bypassSecurityTrustHtml(html);
+      }, 100);
     }
+  }
 
-    constructor(
-        @Attribute('required') public required: boolean = false,
-        @Attribute('maxlength') public maxlength: number = -1,
-        private _domSanitizer: DomSanitizer
-    ) {}
+  constructor(
+    @Attribute('required') public required: boolean = false,
+    @Attribute('maxlength') public maxlength: number = -1,
+    private _domSanitizer: DomSanitizer
+  ) {}
 
-    ngOnInit() {
-        const editorElement = this.aceEditorContainer.nativeElement;
-        this.editor = ace.edit(editorElement);
+  ngOnInit() {
+    const editorElement = this.aceEditorContainer.nativeElement;
+    this.editor = ace.edit(editorElement);
 
-        const markedRender = new marked.Renderer();
-        markedRender.code = (code: any, language: any) => {
-            const validLang = !!(language && hljs.getLanguage(language));
-            const highlighted = validLang
-                ? hljs.highlight(language, code).value
-                : code;
-            return `<pre style="padding: 0; border-radius: 0;"><code class="hljs ${language}">${highlighted}</code></pre>`;
-        };
-        markedRender.table = (header: string, body: string) => {
-            return `<table class="table table-bordered">\n<thead>\n${header}</thead>\n<tbody>\n${body}</tbody>\n</table>\n`;
-        };
-        markedRender.listitem = (text: any) => {
-            if (/^\s*\[[x ]\]\s*/.test(text)) {
-                text = text
-                    .replace(
-                        /^\s*\[ \]\s*/,
-                        '<i class="fa fa-square-o" style="margin: 0 0.2em 0.25em -1.6em;"></i> '
-                    )
-                    .replace(
-                        /^\s*\[x\]\s*/,
-                        '<i class="fa fa-check-square" style="margin: 0 0.2em 0.25em -1.6em;"></i> '
-                    );
-                return `<li style="list-style: none;">${text}</li>`;
-            } else {
-                return `<li>${text}</li>`;
-            }
-        };
-        const markedjsOpt = {
-            renderer: markedRender,
-            highlight: (code: any) => hljs.highlightAuto(code).value,
-        };
-        this._markedOpt = Object.assign(
-            {},
-            this.options.markedjsOpt,
-            markedjsOpt
-        );
+    const markedRender = new marked.Renderer();
+    markedRender.code = markDownCode;
+    markedRender.table = markDownTable;
+    markedRender.listitem = markDownListItem;
+    const markedjsOpt = {
+      renderer: markedRender,
+      highlight: (code: any) => hljs.highlightAuto(code).value,
+    };
+    this._markedOpt = Object.assign({}, this.options.markedjsOpt, markedjsOpt);
+  }
+
+  ngAfterViewInit() {
+    this.editor.$blockScrolling = Infinity;
+    this.editor.getSession().setUseWrapMode(true);
+    this.editor.getSession().setMode('ace/mode/markdown');
+    this.editor.setValue(this.markdownValue || '', 1);
+    this.editor.setOption('scrollPastEnd', this._options.scrollPastEnd || 0);
+
+    this.editor.on('change', () => {
+      this.markdownValue = this.editor.getValue();
+    });
+  }
+
+  writeValue(value: any | Array<any>): void {
+    setTimeout(() => {
+      this.markdownValue = value;
+      if (typeof value !== 'undefined' && this.editor) {
+        this.editor.setValue(value || '', 1);
+      }
+    }, 1);
+  }
+
+  registerOnChange(fn: (_: any) => {}): void {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: () => {}): void {
+    this._onTouched = fn;
+  }
+
+  validate(c: AbstractControl): ValidationErrors {
+    let result: any = null;
+    if (this.required && this.markdownValue.length === 0) {
+      result = { required: true };
     }
-
-    ngAfterViewInit() {
-        this.editor.$blockScrolling = Infinity;
-        this.editor.getSession().setUseWrapMode(true);
-        this.editor.getSession().setMode('ace/mode/markdown');
-        this.editor.setValue(this.markdownValue || '', 1);
-        this.editor.setOption(
-            'scrollPastEnd',
-            this._options.scrollPastEnd || 0
-        );
-
-        this.editor.on('change', () => {
-            this.markdownValue = this.editor.getValue();
-        });
+    if (this.maxlength > 0 && this.markdownValue.length > this.maxlength) {
+      result = { maxlength: true };
     }
+    return result;
+  }
 
-    writeValue(value: any | Array<any>): void {
-        setTimeout(() => {
-            this.markdownValue = value;
-            if (typeof value !== 'undefined' && this.editor) {
-                this.editor.setValue(value || '', 1);
-            }
-        }, 1);
-    }
+  onTogglePreview(isOpen: boolean) {
+    this.showPreviewPanel = isOpen;
+  }
 
-    registerOnChange(fn: (_: any) => {}): void {
-        this._onChange = fn;
-    }
+  onFullScreen(isFullScreen: boolean) {
+    this.isFullScreen = isFullScreen;
+    this.editor.resize();
+  }
 
-    registerOnTouched(fn: () => {}): void {
-        this._onTouched = fn;
+  previewPanelClick(event: Event) {
+    if (this.options.enablePreviewContentClick !== true) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
     }
+  }
 
-    validate(c: AbstractControl): ValidationErrors {
-        let result: any = null;
-        if (this.required && this.markdownValue.length === 0) {
-            result = { required: true };
-        }
-        if (this.maxlength > 0 && this.markdownValue.length > this.maxlength) {
-            result = { maxlength: true };
-        }
-        return result;
-    }
-
-    onTogglePreview(isOpen: boolean) {
-        this.showPreviewPanel = isOpen;
-    }
-
-    onFullScreen(isFullScreen: boolean) {
-        this.isFullScreen = isFullScreen;
-    }
-
-    previewPanelClick(event: Event) {
-        if (this.options.enablePreviewContentClick !== true) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
-        }
-    }
-
-    ngOnDestroy() {
-        return this.editor && this.editor.destroy();
-    }
+  ngOnDestroy() {
+    return this.editor && this.editor.destroy();
+  }
 }
